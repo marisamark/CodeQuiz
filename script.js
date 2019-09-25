20e3d8bbd84c4dac442bd71921735bade1d45eac
Marisa Mark <themarisamark@gmail.com>
2019-09-24 17:42:50 -0700

    adding updates to the index.html file and adding another js file

--- script.js
+++ script.js
@@ -1,57 +1,98 @@
 //Define variables
 var viewhighscores = document.getElementById("viewhighscores");
 var timer = document.getElementById("timer");
-var minutes;
-var timeLeft;
+var timerId, timerId2;
 var start;
 var seconds;
-var timeLeft;
-var score;
-
-//create function for timer 
-var timer = setInterval(
-function prepareQuiz() {
-var timeLeft = 5;
-
-
-//  * Clicking the "Start Quiz" button presents the user with a series of questions. The timer is initialized with a value and immediately begins countdown.
-
-
-
-//conversion miliseconds to minutes 
-var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)); 
-  if (minutes < 10) minutes="0"+minutes;
-//conversion miliseconds to seconds
-var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-  if (seconds < 10) seconds="0"+seconds;
-
-//Display timer on screen
-  document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s"; 
-
-//Notfiy player that there is no time left, move onto next question, stop quiz.
-  if (timeLeft === 0) {
-    clearInterval(timing);
-    document.getElementById("timer").innerHTML = "Time's Up!"; 
-  }
-}, 1000);
-
-//create for loop to go through all questions 
-for (var i = 0; i < questions.length; i++) {
-    let question = questions[i];
-    findAnswer= confirm(question.title);
-     
-    if (findAnswer === question.answer) {
-     score++;
-     
-    alert("Correct" + score);
-    // alert(`You are right! Score ${wins}`);
-    }
-     
-    else {
-    score--;
-    alert("Incorrect!" + score);
-     }
-    }
-
+var timeLeft = 45;
+var score = 0;
+var currentQuestion = 0;
+var title;
+var chocies;
+var answer;
+var alert = document.getElementById("alert");
+var landingDiv = document.getElementById("landing");
+var gameDiv = document.getElementById("game")
 //  * Score is calculated by time remaining. Answering quickly and correctly results in a higher score. Answering incorrectly results in a time penalty (for example, 15 seconds are subtracted from time remaining).
 
+document.getElementById('start').addEventListener("click", start);
+
+function start() {
+  landingDiv.setAttribute("class", "hide");
+  gameDiv.removeAttribute("class");
+  createButtons();
+  timerId = startTimer(1, "timer", timerId);
+}
+
+function createButtons() {
+  let question = questions[currentQuestion];
+  let text1 = document.getElementById("question");
+  text1.textContent = question.title;
+  document.getElementById("choices").innerHTML = '';
+  for (let i = 0; i < question.choices.length; i++) {
+
+    let buttonchoice = document.createElement('button')
+    buttonchoice.setAttribute("class", "button");
+    buttonchoice.setAttribute("value", question.choices[i])
+    buttonchoice.onclick = answerClick
+    // buttonchoice.addEventListener("click", function (event) {
+    //   console.log(event.target)
+    //   currentQuestion++;
+
+    //     createButtons();
+    //   }
+    buttonchoice.textContent = question.choices[i]
+    document.getElementById("choices").appendChild(buttonchoice)
+  }
+
+}
+function answerClick() {
+  if (currentQuestion === questions.length) {
+    return wereDone();
+  }
+
+  if (this.value === questions[currentQuestion].answer) {
+    AdvanceGame(true, "Correct!")
+  } else {
+    AdvanceGame(false, "Wrong!")
+  }
+}
+
+function AdvanceGame(isCorrect, text) {
+  if (isCorrect) {
+    score++;
+  } else {
+    score--
+    timeLeft -= 15;
+  }
+  alert.textContent = text;
+  currentQuestion++;
+  createButtons();
+}
+
+
+function wereDone() {
+}
+
+function quizorder() {
+  questions.title[i]
+}
+
+
+//create function for timer 
+function startTimer(val, id, intervalId) {
+  clearInterval(intervalId)
+  intervalId = setInterval(function () {
+
+    //Display timer on screen
+    timeLeft--;
+    document.getElementById(id).innerHTML = timeLeft;
+
+    //Notfiy player that there is no time left, move onto next question, stop quiz.
+    if (timeLeft === 0) {
+      clearInterval(timerId);
+      document.getElementById(id).innerHTML = "Time's Up!";
+    }
+  }, val * 1000);
+  return intervalId;
+}
